1220fe8bc650429d73289a18ec9366b35446ee2a
Kalim Ahmed <58071152+KIMOO1987@users.noreply.github.com>
2026-04-02 14:51:18 +0500

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,11 +3,13 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    persistSession: true, // MUST BE TRUE
-    storageKey: '3520272430929abcKIMOO', // Custom key to avoid conflicts
-    autoRefreshToken: true,
-    detectSessionInUrl: true
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true, // This is mandatory!
+      autoRefreshToken: true,
+    }
   }
-});
+);
