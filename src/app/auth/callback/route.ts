004b22ddca36ad067b297fbae1fb4137e90dd186
Kalim Ahmed <58071152+KIMOO1987@users.noreply.github.com>
2026-05-02 18:19:04 +0500

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -2,7 +2,7 @@ import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-export const dynamic = 'force-static';
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
   if (process.env.NEXT_EXPORT === 'true') {
