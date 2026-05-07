471a7cba67f4decffc7e74aab70457876ef60e87
Kalim Ahmed <58071152+KIMOO1987@users.noreply.github.com>
2026-05-08 00:23:14 +0500

    Update symbol-mapper.ts

--- src/lib/symbol-mapper.ts
+++ src/lib/symbol-mapper.ts
@@ -4,53 +4,58 @@ export interface SymbolMapEntry {
   oanda: string;
   forexcom: string;
   binance?: string;
+  fcs?: string;
+  goldapi?: string;
+  eodhd?: string;
+  alphavantage?: string;
+  yahoo?: string;
 }
 
 export const SYMBOL_MAP: Record<string, SymbolMapEntry> = {
   // --- FOREX ---
-  "EURUSD": { "tradingview": "FX:EURUSD", "finnhub": "OANDA:EUR_USD", "oanda": "EURUSD", "forexcom": "EURUSD", "binance": "EURUSDT" },
-  "GBPUSD": { "tradingview": "FX:GBPUSD", "finnhub": "OANDA:GBP_USD", "oanda": "GBPUSD", "forexcom": "GBPUSD", "binance": "GBPUSDT" },
-  "USDJPY": { "tradingview": "FX:USDJPY", "finnhub": "OANDA:USD_JPY", "oanda": "USDJPY", "forexcom": "USDJPY" },
-  "GBPJPY": { "tradingview": "FX:GBPJPY", "finnhub": "OANDA:GBP_JPY", "oanda": "GBPJPY", "forexcom": "GBPJPY" },
-  "AUDUSD": { "tradingview": "FX:AUDUSD", "finnhub": "OANDA:AUD_USD", "oanda": "AUDUSD", "forexcom": "AUDUSD", "binance": "AUDUSDT" },
-  "EURJPY": { "tradingview": "FX:EURJPY", "finnhub": "OANDA:EUR_JPY", "oanda": "EURJPY", "forexcom": "EURJPY" },
-  "NZDUSD": { "tradingview": "FX:NZDUSD", "finnhub": "OANDA:NZD_USD", "oanda": "NZDUSD", "forexcom": "NZDUSD", "binance": "NZDUSDT" },
-  "CHFJPY": { "tradingview": "FX:CHFJPY", "finnhub": "OANDA:CHF_JPY", "oanda": "CHFJPY", "forexcom": "CHFJPY" },
-  "USDCAD": { "tradingview": "FX:USDCAD", "finnhub": "OANDA:USD_CAD", "oanda": "USDCAD", "forexcom": "USDCAD" },
-  "USDCHF": { "tradingview": "FX:USDCHF", "finnhub": "OANDA:USD_CHF", "oanda": "USDCHF", "forexcom": "USDCHF" },
+  "EURUSD": { "tradingview": "FX:EURUSD", "finnhub": "OANDA:EUR_USD", "oanda": "EURUSD", "forexcom": "EURUSD", "binance": "EURUSDT", "fcs": "EUR/USD", "alphavantage": "EURUSD" },
+  "GBPUSD": { "tradingview": "FX:GBPUSD", "finnhub": "OANDA:GBP_USD", "oanda": "GBPUSD", "forexcom": "GBPUSD", "binance": "GBPUSDT", "fcs": "GBP/USD", "alphavantage": "GBPUSD" },
+  "USDJPY": { "tradingview": "FX:USDJPY", "finnhub": "OANDA:USD_JPY", "oanda": "USDJPY", "forexcom": "USDJPY", "fcs": "USD/JPY", "alphavantage": "USDJPY" },
+  "GBPJPY": { "tradingview": "FX:GBPJPY", "finnhub": "OANDA:GBP_JPY", "oanda": "GBPJPY", "forexcom": "GBPJPY", "fcs": "GBP/JPY", "alphavantage": "GBPJPY" },
+  "AUDUSD": { "tradingview": "FX:AUDUSD", "finnhub": "OANDA:AUD_USD", "oanda": "AUDUSD", "forexcom": "AUDUSD", "binance": "AUDUSDT", "fcs": "AUD/USD", "alphavantage": "AUDUSD" },
+  "EURJPY": { "tradingview": "FX:EURJPY", "finnhub": "OANDA:EUR_JPY", "oanda": "EURJPY", "forexcom": "EURJPY", "fcs": "EUR/JPY", "alphavantage": "EURJPY" },
+  "NZDUSD": { "tradingview": "FX:NZDUSD", "finnhub": "OANDA:NZD_USD", "oanda": "NZDUSD", "forexcom": "NZDUSD", "binance": "NZDUSDT", "fcs": "NZD/USD", "alphavantage": "NZDUSD" },
+  "CHFJPY": { "tradingview": "FX:CHFJPY", "finnhub": "OANDA:CHF_JPY", "oanda": "CHFJPY", "forexcom": "CHFJPY", "fcs": "CHF/JPY", "alphavantage": "CHFJPY" },
+  "USDCAD": { "tradingview": "FX:USDCAD", "finnhub": "OANDA:USD_CAD", "oanda": "USDCAD", "forexcom": "USDCAD", "fcs": "USD/CAD", "alphavantage": "USDCAD" },
+  "USDCHF": { "tradingview": "FX:USDCHF", "finnhub": "OANDA:USD_CHF", "oanda": "USDCHF", "forexcom": "USDCHF", "fcs": "USD/CHF", "alphavantage": "USDCHF" },
 
   // --- INDICES ---
-  "US100": { "tradingview": "NASDAQ:NDX", "finnhub": "^NDX", "oanda": "NAS100", "forexcom": "US100", "binance": "NDXUSDT" },
-  "US500": { "tradingview": "SP:SPX", "finnhub": "^GSPC", "oanda": "US500", "forexcom": "US500", "binance": "SPXUSDT" },
-  "US30": { "tradingview": "TVC:DJI", "finnhub": "^DJI", "oanda": "US30", "forexcom": "US30", "binance": "DJIUSDT" },
+  "US100": { "tradingview": "NASDAQ:NDX", "finnhub": "^NDX", "oanda": "NAS100", "forexcom": "US100", "binance": "NDXUSDT", "eodhd": "NDX.INDX", "yahoo": "^IXIC" },
+  "US500": { "tradingview": "SP:SPX", "finnhub": "^GSPC", "oanda": "US500", "forexcom": "US500", "binance": "SPXUSDT", "eodhd": "GSPC.INDX", "yahoo": "^GSPC" },
+  "US30": { "tradingview": "TVC:DJI", "finnhub": "^DJI", "oanda": "US30", "forexcom": "US30", "binance": "DJIUSDT", "eodhd": "DJI.INDX", "yahoo": "^DJI" },
 
   // --- METALS ---
-  "XAUUSD": { "tradingview": "XAUUSD", "finnhub": "OANDA:XAU_USD", "oanda": "XAUUSD", "forexcom": "XAUUSD", "binance": "XAUUSDT" },
-  "XAGUSD": { "tradingview": "XAGUSD", "finnhub": "OANDA:XAG_USD", "oanda": "XAGUSD", "forexcom": "XAGUSD", "binance": "XAGUSDT" },
-  "XPTUSD": { "tradingview": "XPTUSD", "finnhub": "OANDA:XPT_USD", "oanda": "XPTUSD", "forexcom": "XPTUSD" },
-  "XCUUSD": { "tradingview": "XCUUSD", "finnhub": "OANDA:XCU_USD", "oanda": "XCUUSD", "forexcom": "XCUUSD" },
+  "XAUUSD": { "tradingview": "XAUUSD", "finnhub": "OANDA:XAU_USD", "oanda": "XAUUSD", "forexcom": "XAUUSD", "binance": "XAUUSDT", "goldapi": "XAU", "alphavantage": "GOLD" },
+  "XAGUSD": { "tradingview": "XAGUSD", "finnhub": "OANDA:XAG_USD", "oanda": "XAGUSD", "forexcom": "XAGUSD", "binance": "XAGUSDT", "goldapi": "XAG", "alphavantage": "SILVER" },
+  "XPTUSD": { "tradingview": "XPTUSD", "finnhub": "OANDA:XPT_USD", "oanda": "XPTUSD", "forexcom": "XPTUSD", "goldapi": "XPT" },
+  "XCUUSD": { "tradingview": "XCUUSD", "finnhub": "OANDA:XCU_USD", "oanda": "XCUUSD", "forexcom": "XCUUSD", "goldapi": "XCU" },
 
   // --- CRYPTO ---
-  "BTCUSD": { "tradingview": "BINANCE:BTCUSDT", "finnhub": "BINANCE:BTCUSDT", "oanda": "BTCUSD", "forexcom": "BTCUSD", "binance": "BTCUSDT" },
-  "ETHUSD": { "tradingview": "BINANCE:ETHUSDT", "finnhub": "BINANCE:ETHUSDT", "oanda": "ETHUSD", "forexcom": "ETHUSD", "binance": "ETHUSDT" },
-  "SOLUSD": { "tradingview": "BINANCE:SOLUSDT", "finnhub": "BINANCE:SOLUSDT", "oanda": "SOLUSD", "forexcom": "SOLUSD", "binance": "SOLUSDT" },
-  "XRPUSD": { "tradingview": "BINANCE:XRPUSDT", "finnhub": "BINANCE:XRPUSDT", "oanda": "XRPUSD", "forexcom": "XRPUSD", "binance": "XRPUSDT" },
-  "BNBUSD": { "tradingview": "BINANCE:BNBUSDT", "finnhub": "BINANCE:BNBUSDT", "oanda": "BNBUSD", "forexcom": "BNBUSD", "binance": "BNBUSDT" },
+  "BTCUSD": { "tradingview": "BINANCE:BTCUSDT", "finnhub": "BINANCE:BTCUSDT", "oanda": "BTCUSD", "forexcom": "BTCUSD", "binance": "BTCUSDT", "alphavantage": "BTC", "yahoo": "BTC-USD" },
+  "ETHUSD": { "tradingview": "BINANCE:ETHUSDT", "finnhub": "BINANCE:ETHUSDT", "oanda": "ETHUSD", "forexcom": "ETHUSD", "binance": "ETHUSDT", "alphavantage": "ETH", "yahoo": "ETH-USD" },
+  "SOLUSD": { "tradingview": "BINANCE:SOLUSDT", "finnhub": "BINANCE:SOLUSDT", "oanda": "SOLUSD", "forexcom": "SOLUSD", "binance": "SOLUSDT", "alphavantage": "SOL", "yahoo": "SOL-USD" },
+  "XRPUSD": { "tradingview": "BINANCE:XRPUSDT", "finnhub": "BINANCE:XRPUSDT", "oanda": "XRPUSD", "forexcom": "XRPUSD", "binance": "XRPUSDT", "alphavantage": "XRP", "yahoo": "XRP-USD" },
+  "BNBUSD": { "tradingview": "BINANCE:BNBUSDT", "finnhub": "BINANCE:BNBUSDT", "oanda": "BNBUSD", "forexcom": "BNBUSD", "binance": "BNBUSDT", "alphavantage": "BNB", "yahoo": "BNB-USD" },
   "TAOUSD": { "tradingview": "BINANCE:TAOUSDT", "finnhub": "BINANCE:TAOUSDT", "oanda": "TAOUSD", "forexcom": "TAOUSD", "binance": "TAOUSDT" },
-  "ADAUSD": { "tradingview": "BINANCE:ADAUSDT", "finnhub": "BINANCE:ADAUSDT", "oanda": "ADAUSD", "forexcom": "ADAUSD", "binance": "ADAUSDT" },
-  "DOGEUSD": { "tradingview": "BINANCE:DOGEUSDT", "finnhub": "BINANCE:DOGEUSDT", "oanda": "DOGEUSD", "forexcom": "DOGEUSD", "binance": "DOGEUSDT" },
-  "AVAXUSD": { "tradingview": "BINANCE:AVAXUSDT", "finnhub": "BINANCE:AVAXUSDT", "oanda": "AVAXUSD", "forexcom": "AVAXUSD", "binance": "AVAXUSDT" },
-  "DOTUSD": { "tradingview": "BINANCE:DOTUSDT", "finnhub": "BINANCE:DOTUSDT", "oanda": "DOTUSD", "forexcom": "DOTUSD", "binance": "DOTUSDT" },
-  "NEARUSD": { "tradingview": "BINANCE:NEARUSDT", "finnhub": "BINANCE:NEARUSDT", "oanda": "NEARUSD", "forexcom": "NEARUSD", "binance": "NEARUSDT" },
-  "LTCUSD": { "tradingview": "BINANCE:LTCUSDT", "finnhub": "BINANCE:LTCUSDT", "oanda": "LTCUSD", "forexcom": "LTCUSD", "binance": "LTCUSDT" },
-  "TRXUSD": { "tradingview": "BINANCE:TRXUSDT", "finnhub": "BINANCE:TRXUSDT", "oanda": "TRXUSD", "forexcom": "TRXUSD", "binance": "TRXUSDT" },
-  "LINKUSD": { "tradingview": "BINANCE:LINKUSDT", "finnhub": "BINANCE:LINKUSDT", "oanda": "LINKUSD", "forexcom": "LINKUSD", "binance": "LINKUSDT" },
-  "BCHUSD": { "tradingview": "BINANCE:BCHUSDT", "finnhub": "BINANCE:BCHUSDT", "oanda": "BCHUSD", "forexcom": "BCHUSD", "binance": "BCHUSDT" },
-  "ATOMUSD": { "tradingview": "BINANCE:ATOMUSDT", "finnhub": "BINANCE:ATOMUSDT", "oanda": "ATOMUSD", "forexcom": "ATOMUSD", "binance": "ATOMUSDT" },
-  "UNIUSD": { "tradingview": "BINANCE:UNIUSDT", "finnhub": "BINANCE:UNIUSDT", "oanda": "UNIUSD", "forexcom": "UNIUSD", "binance": "UNIUSDT" },
-  "APTUSD": { "tradingview": "BINANCE:APTUSDT", "finnhub": "BINANCE:APTUSDT", "oanda": "APTUSD", "forexcom": "APTUSD", "binance": "APTUSDT" },
-  "INJUSD": { "tradingview": "BINANCE:INJUSDT", "finnhub": "BINANCE:INJUSDT", "oanda": "INJUSD", "forexcom": "INJUSD", "binance": "INJUSDT" },
-  "OPUSD": { "tradingview": "BINANCE:OPUSDT", "finnhub": "BINANCE:OPUSDT", "oanda": "OPUSD", "forexcom": "OPUSD", "binance": "OPUSDT" }
+  "ADAUSD": { "tradingview": "BINANCE:ADAUSDT", "finnhub": "BINANCE:ADAUSDT", "oanda": "ADAUSD", "forexcom": "ADAUSD", "binance": "ADAUSDT", "alphavantage": "ADA", "yahoo": "ADA-USD" },
+  "DOGEUSD": { "tradingview": "BINANCE:DOGEUSDT", "finnhub": "BINANCE:DOGEUSDT", "oanda": "DOGEUSD", "forexcom": "DOGEUSD", "binance": "DOGEUSDT", "alphavantage": "DOGE", "yahoo": "DOGE-USD" },
+  "AVAXUSD": { "tradingview": "BINANCE:AVAXUSDT", "finnhub": "BINANCE:AVAXUSDT", "oanda": "AVAXUSD", "forexcom": "AVAXUSD", "binance": "AVAXUSDT", "alphavantage": "AVAX", "yahoo": "AVAX-USD" },
+  "DOTUSD": { "tradingview": "BINANCE:DOTUSDT", "finnhub": "BINANCE:DOTUSDT", "oanda": "DOTUSD", "forexcom": "DOTUSD", "binance": "DOTUSDT", "alphavantage": "DOT", "yahoo": "DOT-USD" },
+  "NEARUSD": { "tradingview": "BINANCE:NEARUSDT", "finnhub": "BINANCE:NEARUSDT", "oanda": "NEARUSD", "forexcom": "NEARUSD", "binance": "NEARUSDT", "alphavantage": "NEAR", "yahoo": "NEAR-USD" },
+  "LTCUSD": { "tradingview": "BINANCE:LTCUSDT", "finnhub": "BINANCE:LTCUSDT", "oanda": "LTCUSD", "forexcom": "LTCUSD", "binance": "LTCUSDT", "alphavantage": "LTC", "yahoo": "LTC-USD" },
+  "TRXUSD": { "tradingview": "BINANCE:TRXUSDT", "finnhub": "BINANCE:TRXUSDT", "oanda": "TRXUSD", "forexcom": "TRXUSD", "binance": "TRXUSDT", "alphavantage": "TRX", "yahoo": "TRX-USD" },
+  "LINKUSD": { "tradingview": "BINANCE:LINKUSDT", "finnhub": "BINANCE:LINKUSDT", "oanda": "LINKUSD", "forexcom": "LINKUSD", "binance": "LINKUSDT", "alphavantage": "LINK", "yahoo": "LINK-USD" },
+  "BCHUSD": { "tradingview": "BINANCE:BCHUSDT", "finnhub": "BINANCE:BCHUSDT", "oanda": "BCHUSD", "forexcom": "BCHUSD", "binance": "BCHUSDT", "alphavantage": "BCH", "yahoo": "BCH-USD" },
+  "ATOMUSD": { "tradingview": "BINANCE:ATOMUSDT", "finnhub": "BINANCE:ATOMUSDT", "oanda": "ATOMUSD", "forexcom": "ATOMUSD", "binance": "ATOMUSDT", "alphavantage": "ATOM", "yahoo": "ATOM-USD" },
+  "UNIUSD": { "tradingview": "BINANCE:UNIUSDT", "finnhub": "BINANCE:UNIUSDT", "oanda": "UNIUSD", "forexcom": "UNIUSD", "binance": "UNIUSDT", "alphavantage": "UNI", "yahoo": "UNI-USD" },
+  "APTUSD": { "tradingview": "BINANCE:APTUSDT", "finnhub": "BINANCE:APTUSDT", "oanda": "APTUSD", "forexcom": "APTUSD", "binance": "APTUSDT", "alphavantage": "APT", "yahoo": "APT-USD" },
+  "INJUSD": { "tradingview": "BINANCE:INJUSDT", "finnhub": "BINANCE:INJUSDT", "oanda": "INJUSD", "forexcom": "INJUSD", "binance": "INJUSDT", "alphavantage": "INJ", "yahoo": "INJ-USD" },
+  "OPUSD": { "tradingview": "BINANCE:OPUSDT", "finnhub": "BINANCE:OPUSDT", "oanda": "OPUSD", "forexcom": "OPUSD", "binance": "OPUSDT", "alphavantage": "OP", "yahoo": "OP-USD" }
 };
 
 /**
